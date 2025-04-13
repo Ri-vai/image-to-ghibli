2f60105f71e1651bde45cbbaf27f341693e1aae2
Zane <zanereed596@gmail.com>
2025-04-13 18:19:12 +0800

    增强多语言支持，通过向区域配置文件添加额外语言（阿拉伯语、德语、西班牙语、法语、意大利语、日语、韩语、荷兰语、葡萄牙语、俄语、土耳其语、繁体中文和越南语），提升用户可访问性和体验。

--- i18n/locale.ts
+++ i18n/locale.ts
@@ -1,10 +1,39 @@
 import { Pathnames } from "next-intl/routing";
 
-export const locales = ["en", "zh"];
+export const locales = [
+  "en",
+  "zh",
+  "ar",
+  "de",
+  "es",
+  "fr",
+  "it",
+  "ja",
+  "ko",
+  "nl",
+  "pt",
+  "ru",
+  "tr",
+  "tw",
+  "vi",
+];
 
 export const localeNames: any = {
   en: "English",
   zh: "中文",
+  ar: "العربية",
+  de: "Deutsch",
+  es: "Español",
+  fr: "Français",
+  it: "Italiano",
+  ja: "日本語",
+  ko: "한국어",
+  nl: "Nederlands",
+  pt: "Português",
+  ru: "Русский",
+  tr: "Türkçe",
+  tw: "繁體中文",
+  vi: "Tiếng Việt",
 };
 
 export const defaultLocale = "en";
