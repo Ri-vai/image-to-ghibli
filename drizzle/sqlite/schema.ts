5c06633fb3eb15621775fea9db2b9aa68635a91b
Zane <zanereed596@gmail.com>
2025-04-20 12:54:59 +0800

    将 db:drop 命令添加到 package.json 并重构数据库模式以包含订阅和增强的订单详情

--- drizzle/sqlite/schema.ts
+++ drizzle/sqlite/schema.ts
@@ -3,7 +3,15 @@ import {
   sqliteTable,
   text,
   uniqueIndex,
+  real,
+  index,
 } from "drizzle-orm/sqlite-core";
+
+export type SubscriptionCycle = "monthly" | "yearly";
+export type ChargeType = "subscription" | "renewal" | "one-time" | "upgrade";
+export type PaymentStatus = "paid" | "refunded";
+export type SubscriptionStatus = "active" | "cancelled" | "cancelling";
+export type PaymentChannel = "Stripe" | "Paypro" | "Creem" | "LemonSqueezy";
 
 export const users = sqliteTable(
   "users",
@@ -29,33 +37,75 @@ export const users = sqliteTable(
   ]
 );
 
-export const orders = sqliteTable("orders", {
-  id: integer().primaryKey({ autoIncrement: true }),
-  order_no: text().unique().notNull(),
-  created_at: text(),
-  user_uuid: text().notNull(),
-  user_email: text().notNull(),
-  amount: integer().notNull(),
-  interval: text(),
-  expired_at: text(),
-  status: text().notNull(),
-  stripe_session_id: text(),
-  credits: integer().notNull(),
-  currency: text(),
-  sub_id: text(),
-  sub_interval_count: integer(),
-  sub_cycle_anchor: integer(),
-  sub_period_end: integer(),
-  sub_period_start: integer(),
-  sub_times: integer(),
-  product_id: text(),
-  product_name: text(),
-  valid_months: integer(),
-  order_detail: text(),
-  paid_at: text(),
-  paid_email: text(),
-  paid_detail: text(),
-});
+export const subscription = sqliteTable(
+  "subscriptions",
+  {
+    id: integer().primaryKey({ autoIncrement: true }),
+    sub_id: text().unique().notNull(),
+    user_uuid: text().notNull(),
+    user_email: text().notNull(),
+    sub_expires_at: text(),
+    sub_status: text({
+      enum: ["active", "cancelled", "cancelling"],
+    }).$type<SubscriptionStatus>(),
+    customer_id: text(),
+    plan_type: text(),
+    cycle: text({ enum: ["monthly", "yearly"] }).$type<SubscriptionCycle>(),
+    payment_channel: text({
+      enum: ["Stripe", "Paypro", "Creem", "LemonSqueezy"],
+    }).$type<PaymentChannel>(),
+    created_at: text().$defaultFn(() => new Date().toISOString()),
+    updated_at: text().$defaultFn(() => new Date().toISOString()),
+  },
+  (table) => {
+    return {
+      emailIndex: index("subscription_email_idx").on(table.user_email),
+      customerIdIndex: index("subscription_customerId_idx").on(
+        table.customer_id
+      ),
+      subIdIndex: index("subscription_subId_idx").on(table.sub_id),
+    };
+  }
+);
+
+export const orders = sqliteTable(
+  "orders",
+  {
+    id: integer().primaryKey({ autoIncrement: true }),
+    order_no: text().unique().notNull(),
+    user_uuid: text().notNull(),
+    user_email: text().notNull(),
+    customer_id: text(),
+    amount: real(),
+    net_amount: real(),
+    charge_type: text({
+      enum: ["subscription", "renewal", "one-time", "upgrade"],
+    })
+      .$type<ChargeType>()
+      .notNull(),
+    payment_country: text(),
+    payment_channel: text({
+      enum: ["Stripe", "Paypro", "Creem", "LemonSqueezy"],
+    })
+      .$type<PaymentChannel>()
+      .notNull(),
+    status: text({ enum: ["paid", "refunded"] })
+      .$type<PaymentStatus>()
+      .notNull(),
+    last_refund_at: text(),
+    total_refund_amount: real(),
+    invoice: text(),
+    paid_at: text(),
+    created_at: text().$defaultFn(() => new Date().toISOString()),
+    updated_at: text().$defaultFn(() => new Date().toISOString()),
+  },
+  (table) => {
+    return {
+      emailIndex: index("order_email_idx").on(table.user_email),
+      paymentIdIndex: index("order_paymentId_idx").on(table.payment_channel),
+    };
+  }
+);
 
 export const apikeys = sqliteTable("apikeys", {
   id: integer().primaryKey({ autoIncrement: true }),
